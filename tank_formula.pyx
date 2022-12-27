f65fc92f16736bc1968491dc85ce7ca7b654ee8c
Cajetan Chinonso <echinonso8@gmail.com>
2022-12-27 17:59:21 +0100

    Tank Calculator Using Cython

--- tank_formula.pyx
+++ tank_formula.pyx
@@ -2,70 +2,69 @@ import math
 
 
 def HorCap():
-    cdef unsigned long int l = cdef unsigned long int(input("Side Length Dimension: "))
-    d = int(input("Diameter Dimension: "))
-    f = int(input("Filled Depth: "))
-    r = d/2
-    e = r - f
+    cdef int l = int(input("Side Length Dimension: "))
+    cdef int d = int(input("Diameter Dimension: "))
+    cdef int f = int(input("Filled Depth: "))
+    cdef double r = d/2
+    cdef double e = r - f
 
-    tank_horvol = math.floor((math.pi * r ** 2 * l) / 1000)
-    theta = 2 * math.acos(e / r)
-    seg_area = 1 / 2 * r ** 2 * (theta - math.sin(theta)) * l / 1000
+    cdef double tank_horvol = math.floor((math.pi * r ** 2 * l) / 1000)
+    cdef double theta = 2 * math.acos(e / r)
+    cdef double seg_area = 1 / 2 * r ** 2 * (theta - math.sin(theta)) * l / 1000
 
     if f <= r:
-        filled_horvol = math.floor(seg_area)
+        cdef double filled_horvol = math.floor(seg_area)
 
     elif f > r:
-        empty_segment = math.floor(tank_horvol - seg_area)
-        filled_horvol = tank_horvol - empty_segment
+        cdef double empty_segment = math.floor(tank_horvol - seg_area)
+        cdef double filled_horvol = tank_horvol - empty_segment
                 
-        tank_vol = math.pi * r ** 2 * ((4 / 3) * r + l)
+        cdef double tank_vol = math.pi * r ** 2 * ((4 / 3) * r + l)
         tank_vol = math.floor(tank_vol / 1000)
 
-        v_sphere = (1 / 3) * math.pi * f ** 2 * (3 * r - f)
+        cdef double v_sphere = (1 / 3) * math.pi * f ** 2 * (3 * r - f)
         v_sphere = math.floor(v_sphere / 1000)
                     
-        filled_vol = v_sphere + filled_horvol
+        cdef double filled_vol = v_sphere + filled_horvol
 
     return filled_vol
 
 
 def HorCyl():
-    l = int(input("Side Length Dimension: "))
-    d = int(input("Diameter Dimension: "))
-    f = int(input("Filled Depth: "))
-    r = d / 2
-    e = r - f
+    cdef int l = int(input("Side Length Dimension: "))
+    cdef int d = int(input("Diameter Dimension: "))
+    cdef int f = int(input("Filled Depth: "))
+    cdef double r = d / 2
+    cdef double e = r - f
 
-    tank_vol = math.floor((math.pi * r ** 2 * l) / 1000)
-    theta = 2 * math.acos(e / r)
-    seg_area = 1 / 2 * r ** 2 * (theta - math.sin(theta)) * l / 1000
+    cdef double tank_vol = math.floor((math.pi * r ** 2 * l) / 1000)
+    cdef double theta = 2 * math.acos(e / r)
+    cdef double seg_area = 1 / 2 * r ** 2 * (theta - math.sin(theta)) * l / 1000
 
     if f <= r:
-        filled_vol = math.floor(seg_area)
+        cdef double filled_vol = math.floor(seg_area)
         print(filled_vol)
     elif f > r:
-        empty_segment = math.floor(tank_vol - seg_area)
+        cdef double empty_segment = math.floor(tank_vol - seg_area)
         filled_vol = tank_vol - empty_segment
 
     return filled_vol
 
 
 def VerCap():
-    l = int(input("Tank Length: "))
-    d = int(input("Tank Diameter: "))
-    f = int(input("Filled Depth: "))
-    r = d / 2
-    h = l + d
-    f_h = h - f
+    cdef int l = int(input("Tank Length: "))
+    cdef int d = int(input("Tank Diameter: "))
+    cdef int f = int(input("Filled Depth: "))
+    cdef double r = d / 2
+    cdef int h = l + d
+    cdef int f_h = h - f
 
-    tank_vol = math.pi * r ** 2 * ((4 / 3) * r + l)
-    tank_vol = math.floor(tank_vol / 1000)
-    v_sphere = ((1 / 3) * math.pi * f ** 2 * (3 * r - f)) / 1000
-    vs_sphere = ((1 / 3) * math.pi * f_h ** 2 * (3 * r - f_h)) / 1000
+    cdef double tank_vol = (math.pi * r ** 2 * ((4 / 3) * r + l))/1000
+    cdef double v_sphere = ((1 / 3) * math.pi * f ** 2 * (3 * r - f)) / 1000
+    cdef double vs_sphere = ((1 / 3) * math.pi * f_h ** 2 * (3 * r - f_h)) / 1000
 
     if f <= r:
-        filled_vol = v_sphere
+        cdef double filled_vol = v_sphere
         filled_vol = math.floor(filled_vol / 1000)
 
     elif r < f <= (r + l):
@@ -79,43 +78,43 @@ def VerCap():
 
 
 def VerCyl():
-    h = int(input("Tank Height: "))
-    d = int(input("Tank Diameter: "))
-    r = d / 2
-    f = int(input("Filled Height: "))
-    tank_volume = (3.14 * r ** 2 * h) / 1000
-    filled_vol = (3.14 * r ** 2 * f) / 1000
+    cdef int h = int(input("Tank Height: "))
+    cdef int d = int(input("Tank Diameter: "))
+    cdef double r = d / 2
+    cdef int f = int(input("Filled Height: "))
+    cdef double tank_volume = (3.14 * r ** 2 * h) / 1000
+    cdef double filled_vol = (3.14 * r ** 2 * f) / 1000
 
     return filled_vol
 
 
 def Rectangular():
-    h = int(input("Tank Height: "))
-    l = int(input("Tank Length: "))
-    w = int(input("Tank Width: "))
-    f = int(input("Filled Height: "))
+    cdef int h = int(input("Tank Height: "))
+    cdef int l = int(input("Tank Length: "))
+    cdef int w = int(input("Tank Width: "))
+    cdef int f = int(input("Filled Height: "))
 
-    tank_vol = (l * h * w) / 1000
-    filled_vol = (l * f * w) / 1000
+    cdef double tank_vol = (l * h * w) / 1000
+    cdef double filled_vol = (l * f * w) / 1000
 
     return filled_vol
 
 
 def HorDish():
-    l = int(input("Tank Length: "))
-    d = int(input("Tank Diameter: "))
-    f = int(input("Filled Depth: "))
-    r = d / 2
-    e = r - f
+    cdef int l = int(input("Tank Length: "))
+    cdef int d = int(input("Tank Diameter: "))
+    cdef int f = int(input("Filled Depth: "))
+    cdef double r = d / 2
+    cdef int e = r - f
 
-    tank_vol = math.floor((math.pi * r ** 2 * l) / 1000)
-    theta = 2 * math.acos(e / r)
-    seg_area = 1 / 2 * r ** 2 * (theta - math.sin(theta)) * l / 1000
+    cdef double tank_vol = math.floor((math.pi * r ** 2 * l) / 1000)
+    cdef double theta = 2 * math.acos(e / r)
+    cdef double seg_area = 1 / 2 * r ** 2 * (theta - math.sin(theta)) * l / 1000
 
     if f <= r:
-        filled_vol = math.floor(seg_area)
+        cdef double filled_vol = math.floor(seg_area)
     elif f > r:
-        empty_segment = math.floor(tank_vol - seg_area)
+        cdef double empty_segment = math.floor(tank_vol - seg_area)
         filled_vol = tank_vol - empty_segment
 
     return filled_vol
